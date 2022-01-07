d48e1e10958eb8eb81b2d433ba29d4e633892841
Reese Ganaway <reeseganaway@gmail.com>
2022-01-07 01:29:39 -0500

    Created the html file, the javascript file, and the css file, and completed the first problem

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+function leftButton(){
+    document.getElementById('buttonArgument').innerText="I'm right"
+}
+
+function rightButton(){
+    document.getElementById('buttonArgument').innerText="No, I'm right!"
+}
